7d3afe5eb1d80556dca87e52bc7f12a2a8a4545f
Mitra <Mitra@Portatil>
2013-05-29 11:51:31 +0200

    29/05/2013 12:00

--- src/Carta/CartaBundle/Controller/DefaultController.php
+++ src/Carta/CartaBundle/Controller/DefaultController.php
@@ -2,28 +2,24 @@
 
 namespace Carta\CartaBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 
 
 class DefaultController extends Controller
 {
     
-   /**
-     * Pagina Principal
-     *
-     * @Route("/", name="pagina")
-     * @Method("GET")
-     * @Template("CartaCartaBundle:Default:menu.html.twig")
-     */
+  
     
     
     public function indexAction()
     {
-        return array();
+         return $this->render('CartaCartaBundle:Default:index.html.twig');
+    }
+    
+    public function platosAction() {
+         return $this->render('CartaCartaBundle:Default:platos.html.twig');
     }
 }
